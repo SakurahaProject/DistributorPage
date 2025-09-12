a9a7e6f05fc22f80d735b221c128c9349182c63e
Kamioda <kamiodamasato@outlook.com>
2025-09-12 19:52:29 +0900

    ページの表示内容を修正

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service');
-module.exports = defineConfig({
-    transpileDependencies: true,
-});
+module.exports = {
+    ...defineConfig({
+        transpileDependencies: true,
+    }),
+    pages: {
+        index: {
+            title: '桜羽ありす - Alice Sakura',
+        },
+    },
+};
